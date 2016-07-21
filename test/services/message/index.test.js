4e27fe312e6b3446e20f3b8da347225dcee0453d
David Kumru <davidkumru@gmail.com>
2016-07-21 11:39:35 +0200

    message service test

--- test/services/message/index.test.js
+++ test/services/message/index.test.js
@@ -2,9 +2,19 @@
 
 const assert = require('assert');
 const app = require('../../../src/app');
+//const reactApp = require('../../../public/app.jsx');
+const messageService = app.service('messages');
+const testInput = 'My first review!';
 
 describe('message service', function() {
   it('registered the messages service', () => {
     assert.ok(app.service('messages'));
+  });
+
+  //test authorization/log in first?
+  it('created a new message on input', () => {
+    assert.equal(messageService.create({
+      text: testInput
+    }), true);
   });
 });
